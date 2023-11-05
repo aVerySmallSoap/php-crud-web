f53c54c444328394c5cd5aad5e61db0e558a5cc7
Lirys <iamyourmarkieboo@gmail.com>
2023-11-05 23:16:01 +0800

    Fixed a bug where delete banner was not showing

--- JS/Delete.js
+++ JS/Delete.js
@@ -12,14 +12,20 @@ function deleteEvent(event){
         xhr.onload = function (){
             let res = JSON.parse(xhr.response);
             if(res.Type === "Failed"){
-                createBanner(document.querySelector("[id*=table-]"), "Failed!", 5000);
+                createBanner(
+                    document.querySelector("[id*=table-]"),
+                    "Failed!",
+                    "#C51605",
+                    5000);
             }else{
                 let row = document.querySelector(`table>tbody>[class*="${res.id}"]`);
                 row.style.transition = "ease-out 2000ms";
                 row.style.animation = "item-slide-out 2000ms";
-                createBanner(document.querySelector(
+                createBanner(
+                    document.querySelector(
                     "[class*=table-]"),
                     "Entry successfully deleted!",
+                    "#CECE5A",
                     5000);
                 setTimeout(() => {
                     row.remove();
